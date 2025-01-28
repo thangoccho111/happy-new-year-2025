3e0ec81cb626b52163554681526de449a106a937
ZzDRG_ChauzZ <62010994+thangoccho111@users.noreply.github.com>
2025-01-28 18:21:10 +0700

    Update index.js

--- src/wish/index.js
+++ src/wish/index.js
@@ -23,24 +23,24 @@ function Wish() {
 					<h2>
 						
 
-Năm mới đến như cánh hoa xuân nở,  
-Mở ra một mùa thành công rực rỡ,  
-Chúc anh/chị luôn vui vẻ, sức khỏe dồi dào,  
-Cùng nhau xây dựng những ước mơ vươn cao.
+Năm mới đến như cánh hoa xuân nở</h2><h2>
+Mở ra một mùa thành công rực rỡ </h2><h2>
+Chúc anh/chị luôn vui vẻ, sức khỏe dồi dào  </h2><h2>
+Cùng nhau xây dựng những ước mơ vươn cao.</h2><h2>
 
-Công việc như con đường rộng mở,  
-Cùng nhau đi, cùng nhau vững bước,  
-Chúc cho mỗi ngày làm việc thêm suôn sẻ,  
-Thành công đong đầy, niềm vui tràn ngập.
+Công việc như con đường rộng mở  </h2><h2>
+Cùng nhau đi, cùng nhau vững bước  </h2><h2>
+Chúc cho mỗi ngày làm việc thêm suôn sẻ </h2><h2>
+Thành công đong đầy, niềm vui tràn ngập.</h2><h2>
 
-Gia đình anh/chị như ngôi nhà ấm áp,  
-Năm mới an lành, tràn đầy yêu thương,  
-Mong anh/chị luôn tươi cười, vững vàng tiến bước,  
-Cùng đồng hành, chúng ta chạm tới những đỉnh cao.
+Gia đình anh/chị như ngôi nhà ấm áp  </h2><h2>
+Năm mới an lành, tràn đầy yêu thương  </h2><h2>
+Mong anh/chị luôn tươi cười, vững vàng tiến bước  </h2><h2>
+Cùng đồng hành, chúng ta chạm tới những đỉnh cao.</h2><h2>
 
-Chúc năm 2025 sẽ là một năm tươi sáng,  
-Chúng ta cùng nhau gặt hái nhiều thành công,  
-Chúc anh/chị mọi điều tốt đẹp, may mắn trọn vẹn,  
+Chúc năm 2025 sẽ là một năm tươi sáng  </h2><h2>
+Chúng ta cùng nhau gặt hái nhiều thành công  </h2><h2>
+Chúc anh/chị mọi điều tốt đẹp, may mắn trọn vẹn  </h2><h2>
 Một năm mới an khang, thịnh vượng, tràn đầy niềm vui!
 					</h2>
 				</div>
